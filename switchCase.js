059dbbf971de6920ecaad32d4d9c4d7872ae819a
mr-harikrishnan <mr.harikrishnan85@gmail.com>
2024-09-04 11:36:28 +0530

    Switch Case

---
+++ switchCase.js
@@ -0,0 +1,35 @@
+let subject = 9;
+switch (subject) {
+
+    case 1:
+        console.log("Tamil Marks");
+        break;
+
+    case 2:
+        console.log("English Marks");
+        break;
+
+    case 3:
+        console.log("Maths Marks");
+        break;
+
+    case 4:
+        console.log("Science Marks");
+        break;
+
+    case 5:
+        console.log("Social Marks");
+        break;
+
+    case 6:
+        console.log("Computer Science Marks");
+        break;
+
+    case 7:
+        console.log("GK Marks");
+        break;
+
+    default:
+        console.log("Not Valid");
+        break;
+    }
